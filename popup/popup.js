4763760673df87bce07d4096bd906e92c335cda3
314programs <314programs@gmail.com>
2024-01-13 01:23:20 +0800

    Added audio

---
+++ popup/popup.js
@@ -0,0 +1,43 @@
+//Elements from HTML
+const LimitTimeElement = document.getElementById("LimitTime")
+const LimitButtonElement = document.getElementById("LimitButton")
+const LimitWebsiteElement = document.getElementById("LimitWebsite")
+
+
+const BlockButtonElement = document.getElementById("BlockButton")
+const BlockWebsiteElement = document.getElementById("BlockWebsite")
+
+
+const RemindButtonElement = document.getElementById("RemindButton")
+
+
+const ScoldButtonElement = document.getElementById("ScoldButton")
+const ScoldTimeElement = document.getElementById("ScoldTime")
+const PlayButtonElement = document.getElementById("PlayButton")
+const ScoldListElement = document.querySelector("#ScoldList select")
+const ScoldTextElement = document.getElementById("ScoldText")
+
+
+const SaveButtonElement = document.getElementById("SaveButton")
+
+
+var MyAudios = []
+function stopAllJavaScriptAudio() {
+    MyAudios.forEach(function(audio) {
+        audio.pause(); // Pause the audio
+        audio.currentTime = 0; // Reset the time
+    });
+}
+
+PlayButtonElement.onclick = function(){
+    stopAllJavaScriptAudio();
+    let SelectedOption = ScoldListElement.options[ScoldListElement.selectedIndex];
+    var AudioName = SelectedOption.id;
+    if(AudioName != "None"){
+        var PlayAudio = new Audio("../audio/" + AudioName + ".mp3");
+        if(MyAudios.indexOf("../audio/" + AudioName + ".mp3") == -1){
+            MyAudios.push(PlayAudio);
+        }
+        PlayAudio.play();
+    }
+}
