bb70574829237b3422784567eccc02bb3bc6173a
314programs <314programs@gmail.com>
2024-01-13 16:20:09 +0800

    Can save now

--- background.js
+++ background.js
@@ -2,7 +2,18 @@ let data = {
     "events": "onStop/onStart",
     "prefs":{
         "LimitTime": "30", 
-        "ScoldList": "None"
+        "LimitButton:": "Limit off",
+        "LimitWebsite": "",
+
+        "BlockButton": "Block off",
+        "BlockWebsite": "",
+
+        "RemindButton": "Timer on",
+
+        "ScoldButton": "Scolding on",
+        "ScoldTime": "15",
+        "ScoldList": "None",
+        "ScoldText": "You should be productive... NOW!"
     }
 }
 
@@ -11,6 +22,7 @@ chrome.runtime.onMessage.addListener(data => {
     switch(event){
         case("onStart"):
             chrome.storage.local.set(prefs);
+            console.log(prefs);
         default:
             break;
     }
--- popup/popup.js
+++ popup/popup.js
@@ -33,10 +33,61 @@ function stopAllJavaScriptAudio() {
 SaveButtonElement.onclick = () => {
     const prefs = {
         LimitTime: LimitTimeElement.value,
-        ScoldList: ScoldListElement.selectedIndex
+        LimitButton: LimitButtonElement.value,
+        LimitWebsite: LimitWebsiteElement.value,
+        
+        BlockButton: BlockButtonElement.value,
+        BlockWebsite: BlockWebsiteElement.value,
+
+        RemindButton: RemindButtonElement.value,
+
+        ScoldButton: ScoldButtonElement.value,
+        ScoldTime: ScoldTimeElement.value,
+        ScoldList: ScoldListElement.selectedIndex,
+        ScoldText: ScoldTextElement.value
     }
 
     chrome.runtime.sendMessage({event: "onStart", prefs});
+}
+
+function ToggleButton(ButtonElement, On, Off){
+    if(ButtonElement.value == On){
+        ButtonElement.value = Off;
+        ButtonElement.classList.remove("is-success");
+        ButtonElement.classList.add("is-danger");
+    }
+    else{
+        ButtonElement.value = On;
+        ButtonElement.classList.add("is-success");
+        ButtonElement.classList.remove("is-danger");
+    }
+}
+
+function ButtonLoad(ButtonElement, On){
+    if(ButtonElement.value == On){
+        ButtonElement.classList.add("is-success");
+        ButtonElement.classList.remove("is-danger");
+    }
+    else{
+        ButtonElement.classList.remove("is-success");
+        ButtonElement.classList.add("is-danger");
+    }
+}
+
+LimitButtonElement.onclick = function(){
+    ToggleButton(LimitButtonElement, "Limit on", "Limit off");
+}
+
+BlockButtonElement.onclick = function(){
+    ToggleButton(BlockButtonElement, "Block on", "Block off");
+}
+
+RemindButtonElement.onclick = function(){
+    ToggleButton(RemindButtonElement, "Timer on", "Timer off");
+}
+
+ScoldButtonElement.onclick = function(){
+    ToggleButton(ScoldButtonElement, "Scolding on", "Scolding off");
 }
 
 
@@ -54,12 +105,41 @@ PlayButtonElement.onclick = function(){
 }
 
 
-chrome.storage.local.get(["LimitTime", "ScoldList"], (result) => {
-    const {LimitTime, ScoldList} = result;
+chrome.storage.local.get(["LimitTime", "LimitButton", "LimitWebsite", "BlockButton", "BlockWebsite", "RemindButton", "ScoldButton", "ScoldTime", "ScoldList", "ScoldText"], (result) => {
+    const {LimitTime, LimitButton, LimitWebsite, BlockButton, BlockWebsite, RemindButton, ScoldButton, ScoldTime, ScoldList, ScoldText} = result;
     if(LimitTime){
         LimitTimeElement.value = LimitTime;
+    }
+    if(LimitButton){
+        LimitButtonElement.value = LimitButton;
+        ButtonLoad(LimitButtonElement, "Limit on");
+    }
+    if(LimitWebsite){
+        LimitWebsiteElement.value = LimitWebsite;
     }
     if(ScoldList){
         ScoldListElement.selectedIndex = ScoldList;
     }
+    if(BlockButton){
+        BlockButtonElement.value = BlockButton;
+        ButtonLoad(BlockButtonElement, "Block on");
+    }
+    if(BlockWebsite){
+        BlockWebsiteElement.value = BlockWebsite;
+    }
+    if(RemindButton){
+        RemindButtonElement.value = RemindButton;
+        ButtonLoad(RemindButtonElement, "Timer on");
+    }
+    if(ScoldButton){
+        ScoldButtonElement.value = ScoldButton;
+        ButtonLoad(ScoldButtonElement, "Scolding on");
+    }
+    if(ScoldTime){
+        ScoldTimeElement.value = ScoldTime;
+    }
+    if(ScoldText){
+        ScoldTextElement.value = ScoldText;
+    }
+    
 })
